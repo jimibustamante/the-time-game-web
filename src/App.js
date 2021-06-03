864e793ab24a4c3436a3eb1abf9b976cd7fa9524
Jaime Bustamante <jimibustamante@gmail.com>
2021-06-03 14:30:10 -0400

    Avoiding repeating matches in picked options

--- src/App.js
+++ src/App.js
@@ -7,19 +7,38 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
 function App() {
   const [facts, setFacts] = useState([])
   const [options, setOptions] = useState([])
   const [answer, setAnswer] = useState(null)
 
+  
   function pickOptions(list) {
     const optionsIndexes = []
     while (optionsIndexes.length < 2) {
       const index = Math.floor(Math.random() * list.length)
-      if (!optionsIndexes.includes(index)) {
-        optionsIndexes.push(index)
+      if (optionsIndexes.length === 1 && list[optionsIndexes[0]].name.includes(' - ')) {
+        const optionA = list[optionsIndexes[0]].name.split(' - ')
+        const optionB = list[index].name.split(' - ')
+        if (!arraysEqual(optionA, optionB)) optionsIndexes.push(index)
+      } else {
+        if (!optionsIndexes.includes(index)) {
+          optionsIndexes.push(index)
+        }
       }
     }
+
     const opts = optionsIndexes.map((index) => {
       return list[index]
     })
